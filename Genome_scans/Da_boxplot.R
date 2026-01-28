c917532c3cbeeff9eae79a5c9025048052af801f
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2026-01-28 17:13:14 +0000

    Update Da_boxplot.R

--- Genome_scans/Da_boxplot.R
+++ Genome_scans/Da_boxplot.R
@@ -1,3 +1,58 @@
+library(ggplot2)
+library(ggpattern)
+
+setwd("/Users/ybc502/Desktop/Inversion_project/Sliding_windows/")
+
+# Load datasets
+Chetone <- read.table("Da_chetone.txt")
+Chetone$species <- "Chetone"
+
+Eresia <- read.table("Da_eresia.txt")
+Eresia$species <- "Eresia"
+
+Numata <- read.table("Da_numata.txt")
+Numata$species <- "Numata"
+
+# Set gene coordinates for each species (example)
+gene_coords <- list(
+  Chetone = 51039870,   
+  Eresia  = 3346772, 
+  Numata  = 1595580    
+)
+
+# Apply filters for the full inversion 
+Eresia <- Eresia[Eresia$V1 >= 2808389 & Eresia$V1 <= 3352577,]
+Numata <- Numata[Numata$V1 >= 1346000 & Numata$V1 <= 1776500,]
+
+# Create full inversion datasets
+Chetone_full <- Chetone
+Eresia_full  <- Eresia
+Numata_full  <- Numata
+
+# Create truncated datasets up to gene
+Chetone_gene <- Chetone[Chetone$V1 <= gene_coords$Chetone, ]
+Eresia_gene  <- Eresia[Eresia$V1 <= gene_coords$Eresia, ]
+Numata_gene  <- Numata[Numata$V1 <= gene_coords$Numata, ]
+
+# Add region_type column
+Chetone_full$region <- "full_inversion"
+Eresia_full$region  <- "full_inversion"
+Numata_full$region  <- "full_inversion"
+
+Chetone_gene$region <- "common_genes"
+Eresia_gene$region  <- "common_genes"
+Numata_gene$region  <- "common_genes"
+
+# Combine all datasets
+All_sp <- rbind(Chetone_full, Eresia_full, Numata_full,
+                Chetone_gene, Eresia_gene, Numata_gene)
+
+# Rename columns
+colnames(All_sp) <- c("start", "end", "value", "species", "region")
+All_sp$region <- factor(All_sp$region, levels = c("full_inversion", "common_genes"))  # enforce region order
+
+
+
 ggplot(All_sp, aes(
   x = species,
   y = value,
